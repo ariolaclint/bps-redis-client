5dce419b5ddabbae1a89271b3a834f926981cdb7
Clint Ariola <clint@nextixsystems.com>
2022-04-05 16:53:02 +0800

    v0.0.8

--- src/index.js
+++ src/index.js
@@ -1,10 +1,50 @@
-const defaultAwesomeFunction = (name) => {
-    const returnStr = `Hello World! - ${name}`;
-    return returnStr;
-  };
-  
-  const awesomeFunction = () => 'Test 12345';
-  
-  export default defaultAwesomeFunction;
-  
-  export { awesomeFunction };
+'use strict'
+
+const REDIS_CLIENT = process.env.REDIS_CLIENT;
+const REDIS_AUTH = process.env.REDIS_AUTH;
+const redis = require("redis");
+
+class RedisClient {
+     constructor()  {
+       
+        (async () => {
+            this.client = redis.createClient(6380, REDIS_CLIENT,  {auth_pass:REDIS_AUTH, tls: {servername: REDIS_CLIENT}})
+            this.client.on('error', (err) => console.log('Redis Client Error', err));
+            await this.client.connect();
+          })();
+    }
+
+    set(key, value) {
+        this.client.set(key, value)
+    }
+
+    get(key) {
+        return this.client.get(key)
+    }
+
+    delete(key) {
+        return this.client.del(key)
+    }
+}
+
+
+const fnSet = function(key, value)   {
+    let redisClient = new RedisClient()
+    return redisClient.set(key, value)
+}
+
+const fnGet = function(key, value)   {
+    let redisClient = new RedisClient()
+    return redisClient.get(key)
+}
+
+const fnDelete = function(key, value)   {
+    let redisClient = new RedisClient()
+    return redisClient.delete(key)
+}
+
+module.exports = {
+    setCache: fnSet,
+    getCache: fnGet,
+    deleteCache: fnDelete
+};
--- test/index.js
+++ test/index.js
@@ -1,12 +1,49 @@
 let chai = require('chai');
+let { setCache, getCache, deleteCache } = require("./../src/index");
+
 var expect = chai.expect
 
-describe('Awesome test.', () => {
-  it('should test similar name', (done) => {
-    let name = "Clint Ariola"
+describe('BPS Redis Client Test', () => {
 
-    expect(name).to.equal("Clint Ariola");
-    done();
-  });
+    it('should equal to the cache value',  (done)  =>  {
+    
+        let key = "key00123456"
+        let value = "value0011111231345"
+
+        new Promise( async (resolve, reject) => {
+
+            await setCache(key, value)
+
+            let cacheValue = await getCache(key)
+            console.log("key-value:" + key + ":" + cacheValue)
+
+            expect(cacheValue).to.equal(value);
+
+            resolve(true)
+        }).then(() => {
+            done()
+        })
+    }).timeout(5000)
+
+    it('should equal to null after cache deleted',  (done)  =>  {
+    
+        let key = "key00123456"
+        let value = null
+
+        new Promise( async (resolve, reject) => {
+
+            await deleteCache(key)
+
+            let cacheValue = await getCache(key)
+            console.log("key-value:" + key + ":" + cacheValue)
+
+            expect(cacheValue).to.equal(value);
+
+            resolve(true)
+        }).then(() => {
+            done()
+        })
+    }).timeout(5000)
+
 
 });
